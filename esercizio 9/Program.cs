a1e5f4a9b335d2ad1abd9419ff35999c082abf52
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-20 14:00:08 +0100

    Aggiungere i file di progetto.

---
+++ esercizio 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire dimensione");
+            int d=int.Parse(Console.ReadLine());
+            int[] a = new int[d];
+            int[] b=new int[d];
+            for (int i = 0; i < d; i++)
+            {
+                Console.WriteLine("Inserire numero: ");
+                a[i]=int.Parse(Console.ReadLine());
+                inv(a, b, ref d);
+                Console.WriteLine(b);
+            }
+
+        }
+        public static void inv(int []a, int []b, ref int d)
+        {
+            for(int i = 0; i < d; i++)
+            {
+                a[i] = b[d];
+            }
+        }
+    }
+}
